6e0fc2fa9ecb54606743ebac819e442d21259d6c
dani <dani.hodovic1@gmail.com>
2014-01-23 15:49:09 +0100

    Impossible file

--- main.c
+++ main.c
@@ -1,36 +1,43 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-struct linkedlist	{
-	int value;
-	struct linkedlist * next;
+struct bin_tree	{
+	int data;
+	struct bin_tree * left;
+	struct bin_tree * right;
 };
 
-typedef struct linkedlist item;
+typedef struct bin_tree node;
 
-int main(int argc, char **argv) {
-	item * root;
-	item * current;
-
-	root = malloc(sizeof(item));
-	root->value = 500;
-
-	current = root;
-
-	int i;
-	//Here it loops even if the current is never assigned
-	for(i = 0; i <= 20; i++)	{
-		current->next = malloc(sizeof(item));
-		current = current->next;
-		current->value = i;
+node* insert(node * tree, int val)	{
+	if(!tree)	{
+		tree = (node *)malloc(sizeof(node));
+		tree->left = NULL;
+		tree->right = NULL;
+		tree->data = val;
+		return tree;
+	}else if(val < (tree)->data)	{
+		printf("Adding to the left : %d\n", tree->data);
+		insert(tree->left, val);
+	}else if(val > (tree)->data)	{
+		printf("Adding to the right : %d\n", tree->data);
+		insert(tree->right, val);
 	}
-
-	while(root->next != NULL)	{
-		//This is enver executed!
-		printf("[%d]\n", root->value);
-		root = root->next;
-	}
-	printf("Hello\n");
-	return 0;
+	return tree;
 }
 
+void print_inorder(node *tree)	{
+	if(tree)	{
+		print_inorder(tree->right);
+		printf("%d\n", tree->data);
+		print_inorder(tree->left);
+	}
+}
+int main()	{
+	node *root = NULL;
+	root = insert(root, 9);
+	root = insert(root, 4);
+	root = insert(root, 15);
+	print_inorder(root);
+	return 0;
+}
