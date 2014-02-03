44b14d8e37a47b5a0f8976117db969e1e489af58
Dani Hodovic <dani.hodovic1@gmail.com>
2014-02-03 18:29:56 +0100

    Fixed palindrome bug

--- src/exerc_2_4.c
+++ src/exerc_2_4.c
@@ -3,19 +3,21 @@
 #include <string.h>
 
 int main(int argc, char **argv) {
-	char* first_letter = *(argv + 1);
-	char* last_letter = *(argv + 1) + (strlen(*(argv + 1)) - 1);
+	char* word = *(argv + 1);
+	char* first_letter = word;
+	char* last_letter = word + strlen(word) - 1;
+
+	printf("\n%s is ", word);
 
 	while(*first_letter == *last_letter)	{
-		if(first_letter == last_letter)	{
-			puts("Palindrome");
+		if(last_letter - first_letter <= 1)	{
+			printf("palindrome\n");
 			exit(1);
 		}
+
 		first_letter++;
 		last_letter--;
 	}
-	puts("Not palindrome");
-
-
+	printf("not palindrome\n");
 	return 0;
 }
