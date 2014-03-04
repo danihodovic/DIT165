c2082597f16888a64eae2756c839fb1edaba5e30
Dani Hodovic <dani.hodovic1@gmail.com>
2014-03-04 12:26:17 +0100

    Updated the qc, only dates missing

--- ErlangTV/qcdemo2.erl
+++ ErlangTV/qcdemo2.erl
@@ -22,83 +22,35 @@ prop_seq2() -> ?FORALL(
 					   %%(To-From+1)  0
 					   end)).
 
-%% Testing lists:seq/3 example 1
-%% ----------------------------------------
-%% This is the first testing example. Testing for 
-%% inc being positive and incs being negative separately.
-%% It also avoids extreme cases, i.e inc being 0.
-%% This is done by generating the 3 arguments randomly
-%% within certain ranges
-
-prop_lists_seq1() ->
-	prop_seq1_helper(pos),
-	prop_seq1_helper(neg).
-
-prop_seq1_helper(Command) ->
-	?FORALL(
-	{From, To, Inc}, gen_ints(Command),
-	collect({From, Inc, To}, 
-			try
-				List = lists:seq(From, To, Inc),
-				[H|_] = List,
-				
-				%%Test1
-				From == H,
-				%%Test2
-				is_inc(List, Inc) == true
-			catch error:Error -> Error
-			end)).
-
-gen_ints(Command) ->
-	case Command of
-		pos -> gen_positive();
-		neg -> gen_negative()
-	end.
-
-gen_positive() ->
-	Min = -100000,
-	Max = 100000,
-	First = choose(Min, -1), 
-	Last = choose(1, Max * 10),
-	Inc = choose(1, Max * 10),
-	{First, Last, Inc}.
-
-gen_negative() ->
-	Min = -100000,
-	Max = 100000,
-	First = choose(1, Max), 
-	Last = choose(Min * 10, -1),
-	Inc = choose(Min * 10, -1),
-	{First, Last, Inc}.
 
 %% Testing lists:seq/3 example 2
 %% ----------------------------------------
 %% This is the second example following the provided
 %% examples. It verifies the extreme values in the catch
 
-prop_lists_seq2() ->
+expected_fail(From, To, Inc) ->
+	(Inc == 0)
+		or
+		((From > To) and (Inc >= 0))
+		or
+		((To > From) and (Inc =< 0)).
+
+prop_list_seq() ->
 	?FORALL(
 	{From, To, Inc}, {int(), int(), int()},
 	collect(
-	  	(abs(From) > 0) 
-		   and (abs(To) > 1) 
-		   and (abs(Inc) > 0), 
-			try
-				List = lists:seq(From, To, Inc),
-				[H|_] = List,
-				
-				%%Test1
-				From == H,
-				%%Test2
-				is_inc(List, Inc) == true
-			catch error:_ -> 
-				(Inc == 0)
-				or
-				((From > To) and (Inc >= 0))
-				or
-				((To > From) and (Inc =< 0))
-
-			end)).
+	  expected_fail(From, To, Inc), 
+	  try
+		  List = lists:seq(From, To, Inc),
+		  [H|_] = List,
+		  
+		  %%Test1
+		  From == H,
+		  %%Test2
+		  is_inc(List, Inc)
+	  catch error:_ -> 
+				expected_fail(From, To, Inc)
+	  end)).
 
 %% Helper method for testing increment
 is_inc([_|[]], _) ->
@@ -117,32 +69,50 @@ is_inc([H, H2|T], Inc) ->
 %% integers in the mixed one. I was not sure if I 
 %% could supply with only one mixed example so provided
 %% multiple list test
+gen_random_list() ->
+	list(gen_random_item()).
 
-prop_lists() ->
-	prop_2_help(ints),
-	prop_2_help(atoms),
-	prop_2_help(mixed).
+gen_random_item() ->
+	oneof([int(), binary(), char(), bitstring(), bool(),
+				nat(), largeint()]).
 
-gen_list(Command) ->
-	case Command of
-		ints -> lists:seq(1, 10);
-		atoms -> [these, are, all, atoms, lol];
-		mixed -> [{true, tuple}, {"hi", 2}, 
-				  atom, <<1:1,0:1>>, -77]
-	end.
-
-prop_2_help(Command) ->
+prop_list_1() ->
 	?FORALL(
-	{OriginalList}, {gen_list(Command)},
-	collect(OriginalList, 
+	{OriginalList}, {gen_random_list()},
+	collect(OriginalList /= [], 
 			try
-			%% Test1 - Remove nonexist item list is unchanged
+				%% Test1 - Remove nonexist item list is unchanged
 				MutatedList = lists:delete("test", OriginalList),
 				MutatedList == OriginalList,
 				
-			%% Test2 - Remove exist item, list shorter by 1
+				%% Test2 - Remove exist item, list shorter by 1
 				[FirstItem|_] = OriginalList,
 				ShorterList = lists:delete(FirstItem, OriginalList),
 				length(ShorterList) == length(OriginalList) - 1
-			catch error:Error -> Error
+				
+			catch error:_Error -> 
+				OriginalList == []
+			end)).
+
+%% Testing adding and removing random item
+%% ----------------------------------------
+
+item_exists([H|_T], H) ->
+	true;
+item_exists([H|T], Item) ->
+	%io:format("LOOOOOOHead: ~p, Item: ~p~n", [H, Item]),
+	item_exists(T, Item);
+item_exists([], _) ->
+	false.
+
+prop_list_2() ->
+	?FORALL(
+	{OriginalList, Randomitem}, {gen_random_list(), gen_random_item()},
+	collect(item_exists(OriginalList, Randomitem), 
+			try
+				AddedList = OriginalList ++ [Randomitem],
+				OriginalList == AddedList -- [Randomitem]
+			catch error:Error -> 
+				io:format("ERROR IS HERE: ~p~n", [Error])
+				%item_exists(OriginalList, Randomitem)
 			end)).
